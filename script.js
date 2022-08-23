7238e9bd4ba25e83571763ec77c6ad04de127c9d
Dovydoff <70907949+Dovydoff@users.noreply.github.com>
2022-08-23 19:21:20 +0300

    additional drawings

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
 
 let numberOfCars = 0;
 let distance = 0;
-
+let cars = [];
 
 //! 1. function getUserInput (), kuri paims is userio numberOfCars ir distance;
 //! ir pacallinam sukurta funkcija
@@ -28,72 +28,84 @@ let distance = 0;
 
 
 function getUserInput() {
-   do{
-        numberOfCars = +prompt("Enter how much cars you want in race:",5);
-    } while (!(Number.isInteger(numberOfCars) && numberOfCars > 0));
+  do {
+    numberOfCars = +prompt("Enter how much cars you want to race:", 5);
+  } while (!isPositiveInteger(numberOfCars));
 
-    }
-    do{
-        distance = +prompt("Enter distance of track: :",100);
-    } while (!isPositiveInteger(distance));
-
+  do {
+    distance = +prompt("Enter distance of track: :", 100);
+  } while (!isPositiveInteger(distance));
+}
 
 function isPositiveInteger(input) {
-    return Number.isInteger(input) && input >0;
+  return Number.isInteger(input) && input > 0;
 }
-    
+
 
 
 //!2 Sukurti kontruktoriu 
 
 class Car {
-    constructor(id){
-        this.id = id;
-        this.speed = 0;
-        this.distance = 0;
-    }
-    accelerate(howMuch){
-        this.speed += howMuch;
-    }
-    slowDown(howMuch){
-        this.speed -= howMuch;
-        if (this.speed <0) this.speed = 0 
-        
-    }
-    move(){
-        this.distance += this.speed;
-    }
+  constructor(id) {
+    this.id = id;
+    this.speed = 0;
+    this.distance = 0;
+  }
+  accelerate(howMuch) {
+    this.speed += howMuch;
+  }
+  slowDown(howMuch) {
+    this.speed -= howMuch;
+    if (this.speed < 0) this.speed = 0
+
+  }
+  move() {
+    this.distance += this.speed;
+  }
+}
+//console.log(new Car (5));
+
+
+//! 3 function drawCars() kuri nupiesia tiek auto, kiek ivesta userio;
+//! 3.1 Susikuriam tuscia array globaliai cars, i kuri supushinsim tiek
+//! objektu, kiek useris ivede auto lenktynems.
+//! kurti panaudojant klase Car sukti loopa, kol pasieksim numberOfCars;
+//! Kaip parametra i Car klase paduoti array indexa (i);
+//! 3.2 Sukt loopa per visus car array objektus ir kiekvienam is ju
+//! sukurti nauja HTML elementa div, prideti klase 'car' nurodyti pozicija
+//! per .style.top = 60 * car.id + "pixels"
+//! ir appendinti i dokumenta
+
+function drawCars(amount) {
+
+  for (let i = 0; i < amount; i++) {
+    cars.push(new Car(i));
+
+  }
+  //!3.2 nupiesti
+  // cars.forEach(car) => {
+  //create new div;
+  //add class .car;
+  //add style top = 60px * car.id + "px";
+  // append to body
+  // }
+  cars.forEach((car) => {
+    const newCar = document.createElement("div");
+    newCar.classList.add("car");
+    newCar.style.top = 60 * car.id + "px";
+    document.querySelector("body").append(newCar);
+  });
 }
 
 
-//!
-
-console.log(new Car (5));
-getUserInput();
 
 
 
 
 
-
-
-
-// `The car id is : ${this.id} \n\ current speed is : ${this.speed} \n\ 
-//         current distance is : ${this.distance}`
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  getUserInput();
+  drawCars(numberOfCars);
+  console.log(cars);
 
 
 
